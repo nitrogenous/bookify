394ac03366e330595fff3b0b089a21014e19d510
Toprak Koç <hello@toprak.dev>
2020-10-14 09:05:02 +0300

    implemented BookListings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Providers } from "./Providers";
 import SearchField from './Components/SearchField';
+import BookListings from './Containers/BookListings';
 
 function App() {
   return (
     <Providers>
       <SearchField />
+      <BookListings />      
     </Providers>
   );
 }
