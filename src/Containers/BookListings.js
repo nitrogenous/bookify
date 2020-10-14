729f874be3ece9e2a28cee7ee8de705ed528be30
Toprak Koç <hello@toprak.dev>
2020-10-14 09:05:10 +0300

    implemented BookItem

--- src/Containers/BookListings.js
+++ src/Containers/BookListings.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import BookItem from '../Components/BookItem';
 
 const BookListings = () => {
+    return (
+        <BookItem />
+    );
 };
 
 export default BookListings;
