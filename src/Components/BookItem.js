4534abc432aecb1bfb0ac5c5ca26c4ffec0e66c4
Toprak Koç <hello@toprak.dev>
2020-10-14 09:50:36 +0300

    updated bookItem details by dynamic values

--- src/Components/BookItem.js
+++ src/Components/BookItem.js
@@ -1,27 +1,29 @@
 import React from 'react';
 
-const BookItem = () => {
+const BookItem = (props) => {
+    const { details } = props;
+
     return (
         <div className="ui link cards">
             <div className="card">
                 <div className="image">
-                    <img src="http://books.google.com/books/content?id=ZH4oAwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api" />
+                    <img src={!!details.imageLinks ? details.imageLinks.thumbnail : '' } />
                 </div>
                 <div className="content">
-                    <div className="header">Zero To One</div>
+                    <div className="header">{details.title}</div>
                     <div className="meta">
-                        <a>Peter Thiel, Blake Masters</a>
+                        <a>{details.authors}</a>
                     </div>
                     <div className="description">
-                        #1 NEW YORK TIMES BESTSELLER If you want to build a better future, you must believe in secrets.
+                        {details.description}
                     </div>
                 </div>
                 <div className="extra content">
                     <span className="right floated">
-                        Published at 2014.09.16
+                        {details.publishedDate}
                     </span>
                     <span>
-                        Business & Economics
+                        {details.categories}
                     </span>
                 </div>
             </div>
