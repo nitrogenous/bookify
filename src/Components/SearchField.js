b4444f000f45ba63df5f6a78d5c5c8809b8f28c0
Toprak Koç <hello@toprak.dev>
2020-10-14 07:43:07 +0300

    created book name on change function

--- src/Components/SearchField.js
+++ src/Components/SearchField.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { APIContext } from '../Providers/APIProvider';
 
 function SearchField () {
+    const { setBookName } = useContext(APIContext);
+
+    const bookNameOnChange = function (event) {
+        const bookName = event.target.value;
+
+        if(bookName.length > 2) {
+            setBookName(bookName);
+        }
+    };
+
     return (
         <div className='search-field wrapper'>
-            <input className='search-field search-input'/>
+            <input className='search-field search-input' onChange={bookNameOnChange}/>
             <button className='search-field search-button'> Search </button>
         </div>
     );
